d68dad4996d08ba18b693022458f8637f70a92ab
Alessio <alessio.micali@gmail.com>
2024-09-09 03:20:10 +0200

    Add src/types/express/index.d.ts

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,12 @@
+import { JwtPayload } from 'jsonwebtoken';
+import express from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: JwtPayload;
+    }
+  }
+}
+
+export = express;
